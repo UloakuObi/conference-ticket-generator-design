269db1d4cd74ee33168ed0d03f82b7d1b1fe06f8
UloakuObi <uloakuobi@yahoo.com>
2025-11-28 14:39:28 +0100

    Update form logic

--- components/generateCard.js
+++ components/generateCard.js
@@ -1,6 +1,8 @@
 export default function initGenerateCard() {
     const userInfo = JSON.parse(sessionStorage.getItem("userInfo"))
 
+    // console.log(userInfo)
+
     const card = document.querySelector(".card-container")
     const successTitle = document.getElementById("successTitleMsg")
     const ticketConfirmation = document.getElementById("ticketConfirmation")
@@ -8,22 +10,21 @@ export default function initGenerateCard() {
     const githuhUsername = document.getElementById("githubUsername")
     const cardAvatar = document.getElementById("cardAvatar")
 
-    // console.log(userInfo)
-
-    const successTitleMsg = `Congrats, <span class="highlight-name">${userInfo.name}</span>! Your ticket is ready.`
-
-    const ticketConfirmMsg = `
-    We've emailed your ticket to 
-    <span class="red-highlight">${userInfo.email}</span> 
-    and will send updates in the run up to the event.
-    `
-
     if (card) {
+        const successTitleMsg = `Congrats, <span class="highlight-name">${userInfo.name}</span>! Your ticket is ready.`
+
+        const ticketConfirmMsg = `
+        We've emailed your ticket to 
+        <span class="red-highlight">${userInfo.email}</span> 
+        and will send updates in the run up to the event.
+        `
+
         successTitle.innerHTML = successTitleMsg
         ticketConfirmation.innerHTML = ticketConfirmMsg
         cardUsername.textContent = userInfo.name
         githuhUsername.textContent = userInfo.username
         cardAvatar.src = userInfo.avatar
-    }
+    
+}
 
 }
